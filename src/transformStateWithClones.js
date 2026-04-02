cfaf557eaf504254369156d32681748d0797a2bc
Vitalii <vitalilkrutoy234@gmail.com>
2026-04-02 16:07:19 +0300

    Fix eslint/prettier config and implement transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,38 @@
  *
  * @return {Object[]}
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+function transformStateWithClones(initialState, actions) {
+  let prevState = { ...initialState };
+  const history = [];
+
+  for (const action of actions) {
+    let newState;
+
+    switch (action.type) {
+      case 'clear':
+        newState = {};
+        break;
+
+      case 'addProperties':
+        newState = { ...prevState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        newState = { ...prevState };
+        action.keysToRemove.forEach((key) => {
+          delete newState[key];
+        });
+        break;
+
+      default:
+        newState = { ...prevState };
+    }
+
+    history.push(newState);
+    prevState = newState;
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
